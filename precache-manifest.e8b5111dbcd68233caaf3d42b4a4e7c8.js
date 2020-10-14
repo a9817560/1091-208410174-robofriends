a1a55d24f6a9f792f7b3bac4bf4e227e7e070787
a9817560 <a9817560@gmail.com>
2020-10-14 19:05:44 +0800

    Updates

---
+++ precache-manifest.e8b5111dbcd68233caaf3d42b4a4e7c8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dad8503f45389bd75b735c0121d9cee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e9c97b84de84f0d8d725",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "ec6e503b58271fe623c5",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e9c97b84de84f0d8d725",
+    "url": "/static/js/2.0b5b9267.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0b5b9267.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec6e503b58271fe623c5",
+    "url": "/static/js/main.273e0834.chunk.js"
+  },
+  {
+    "revision": "af1ee15f3b8733767315",
+    "url": "/static/js/runtime-main.8a8ac9cd.js"
+  }
+]);
